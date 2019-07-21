b8059418c5b3d56ad88c5193b87dcad2111018a0
Alexander Chebaev <sunychibo@gmail.com>
2019-07-21 18:53:36 +0300

    Initializing commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div className="warning__message">
+                    <h4 className="warning__heading">Warning!</h4>
+                    <div className="warning__content">Are you sure you want to do this</div>
+                </div>
+            </ApprovalCard>    
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 6:00PM"
+                    avatar={faker.image.avatar()}
+                    comment="Nice blog post!"/>
+            </ApprovalCard>    
+            <ApprovalCard>    
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 7:00PM"
+                    avatar={faker.image.avatar()}
+                    comment="Nice blog post!"/>
+            </ApprovalCard>    
+            <ApprovalCard>
+                <CommentDetail
+                    author="Dany"
+                    timeAgo="Today at 8:00PM"
+                    avatar={faker.image.avatar()}
+                    comment="Nice blog post!"/>
+            </ApprovalCard>        
+        </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
